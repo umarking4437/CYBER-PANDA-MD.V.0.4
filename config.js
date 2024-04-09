8ffc72716778c0a3b99cd7d31021b1772b7a38a6
umarking4437 <163032734+umarking4437@users.noreply.github.com>
2024-04-09 06:45:45 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,12 +21,12 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
+global.Owner = ["+923488652755", "+923488652755"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+923488652755", "+923488652755"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+923488652755"];
 global.OwnerName = "SACHITH";
 global.BotName = "SACHITH";
-global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
+global.packname = "🦋 │𝐔𝐌𝐀𝐑│𝐊𝐀𝐇𝐍│🦋";                             //Do not change.
 global.author = "SACHITH";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
